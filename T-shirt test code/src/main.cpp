bfe3dbc4b35a2a2d503e540c8a73c2037b1e0a57
Michael Klingelhoefer <michael.klingelhoefer@mines.sdsmt.edu>
2021-11-13 13:28:13 -0700

    Initial servo test

--- T-shirt test code/src/main.cpp
+++ T-shirt test code/src/main.cpp
@@ -1,7 +1,13 @@
 #include <Arduino.h>
+#include <Servo.h>
 
 void setup() {
   // put your setup code here, to run once:
+  Servo servoL;
+
+  servoL.attach(9);
+
+  servoL.write(100);
 }
 
 void loop() {
